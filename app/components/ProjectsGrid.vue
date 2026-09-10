03477e8f3a80b1e7e2193d2b74939898c0ec88b5
Jisu Lee <jisu_lee@hotmail.de>
2026-09-10 21:05:13 +0200

    better wording on projects section header

--- app/components/ProjectsGrid.vue
+++ app/components/ProjectsGrid.vue
@@ -5,9 +5,9 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="gradient-text">
           <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
         </svg>
-        Featured Projects
+        My Projects
       </h2>
-      <p class="section-subtitle">Key applications and open-source software engineered for high performance</p>
+      <p class="section-subtitle">Personal slack time projects</p>
     </div>
 
     <!-- Projects Grid -->
